021b620811fe2dfd0e0f831e82c09ce64ed9fa9b
Farrukh Karimov <farrukhkarimov@Farrukhs-MacBook-Pro.local>
2022-01-27 14:26:12 -0500

    added simple query

--- index.js
+++ index.js
@@ -1,28 +1,63 @@
 // import our restaurants
-const restaurants = require('./restaurants.json');
+const restaurants = require("./restaurants.json");
 
 // import a set of tolls to talk to Firebase and Firestore
-const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
-const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
+const {
+  initializeApp,
+  applicationDefault,
+  cert,
+} = require("firebase-admin/app");
+const {
+  getFirestore,
+  Timestamp,
+  FieldValue,
+} = require("firebase-admin/firestore");
 
 // import our credentials
-const credentials = require('./credentials.json')
+const credentials = require("./credentials.json");
+const { read } = require("fs");
 
 // connect to Firebase Services
-initializeApp({
-    credential: cert(credentials)
-});
+initializeApp({ credential: cert(credentials) });
 
 // connect to Firestore
 const db = getFirestore();
 
 // create a collection called "restaurants"
+const restRef = db.collection("restaurants");
 
 // add each restaurant
-db.collection('restaurants').add(restaurants[0])
-    .then(doc => {
-        console.log('Added restaurant', doc.id)
-    })
-    .catch(err => {
-        console.error(err)
+// restRef.add(restaurants[3])
+//   .then((doc) => {
+//     console.log("Added restaurant", doc.id);
+//   })
+//   .catch((err) => {
+//     console.error(err);
+//   });
+
+// read one document
+// restRef.doc('LJSTDbFxWwP1EYq2XRjp').get()
+//     .then(doc => {
+//         console.log(doc.id, '=>', doc.data())
+//     })
+//     .catch(err => console.error(err));
+
+// get all documents
+// restRef.get()
+//     .then(snapshot => {
+//         snapshot.forEach(doc => {
+//             console.log(doc.id, '=>', doc.data())
+//         })
+//     })
+//     .catch(console.error)
+
+//querying find a document(s)
+restRef
+  .where("name", "==", "Bolay")
+  .get()
+  .then((snapshot) => {
+    snapshot.forEach((doc) => {
+      console.log(doc.data());
     });
+  })
+  .catch(console.error);
